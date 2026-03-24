9e422adcf2f7e857df5e0dcb4fead5199af08501
Milenakindleindarosa <milena.kindlein.da.da.rosa.ifc@gmail.com>
2026-03-24 09:25:30 -0300

    feat: Criação da API para Cor

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -1,2 +1,3 @@
 from .acessorio import AcessorioSerializer
+from .cor import CorSerializer
 from .user import UserRegistrationSerializer, UserSerializer
--- core/views/__init__.py
+++ core/views/__init__.py
@@ -1,2 +1,3 @@
 from .acessorio import AcessorioViewSet
+from .cor import CorViewSet
 from .user import UserRegistrationView, UserViewSet
